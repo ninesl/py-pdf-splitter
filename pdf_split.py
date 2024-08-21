0e8531aec853e7395f830e40af4d9e5d1eafa6e0
ninesl <lance.nines@gmail.com>
2024-08-21 12:23:08 -0400

    improved logging. Added filecount check for txt files in pdf_to_txt

--- pdf_split.py
+++ pdf_split.py
@@ -6,24 +6,12 @@ import threading
 import os
 import time
 
-FILES = set()
 THREADS = []
 
-def FILES_checker() -> None:
-    def threads_running() -> bool:
-        for thread in THREADS:
-            if thread.is_alive():
-                return True
-            thread.handled = True
-        return False
-    
-    while threads_running():
-        time.sleep(1)
-
-def get_pdf_threads() -> list[threading.Thread]:
+def get_pdf_threads(country_code) -> list[threading.Thread]:
     print(f"{paths.PARENT_DIRECTORY_WSL = }")
     print("Hello world")
-    path_dir = Path.cwd() / f"{paths.PARENT_DIRECTORY_WSL}"
+    path_dir = Path.cwd() / f"{paths.PARENT_DIRECTORY_WSL}" / f"{country_code}"
     
     country_list = [country for country in path_dir.iterdir()]
     threads: list[threading.Thread] = []
@@ -36,13 +24,16 @@ def get_pdf_threads() -> list[threading.Thread]:
 
     return threads
 
+PDF_CODE_COUNT = 0
 def thread_find_pdfs(filepath) -> None:
-    print("Looking through", filepath.name)
+    global PDF_CODE_COUNT
+    PDF_CODE_COUNT += 1
+    print("Looking through", filepath.name, PDF_CODE_COUNT, "pdf_codes found")
     files_found = list(filepath.rglob("*.pdf"))
     for file in files_found:
         pdf_to_txt(file)
-
-    print("\nFound all pdf files for", filepath.name)
+    PDF_CODE_COUNT -= 1
+    print("Found all pdf files for", filepath.name, PDF_CODE_COUNT, "pdf_codes to go")
 
 def pdf_to_txt(file) -> bool:
     pdf_filename: str = file.parts[-1]
@@ -54,17 +45,23 @@ def pdf_to_txt(file) -> bool:
     country_code: str = file.parts[-5]
     day_00: str = pdf_name[-6:-4]
 
-    txt_file_dir: str = f"{paths.PARENT_DIRECTORY_WSL}/{pdf_code}/{year}/{month}/{day_00}"
+    txt_file_dir: str = f"{paths.PARENT_DIRECTORY_WSL}/{country_code}/{pdf_code}/{year}/{month}/{day_00}"
 
     pdf_num = 1
     dir_exists: bool = os.path.isdir(txt_file_dir)
 
-    if dir_exists:
-        return False
     
-    with fitz.open(file) as pdf_file:
+    with fitz.open(file, filetype="pdf") as pdf_file:
         text = "" # concat to this. txt files could be parsed from more than one pdf page
-        os.makedirs(txt_file_dir, exist_ok=True)
+
+        # check if any txt files were missed. hopefully doesn't bork everything
+        # will rewrite over pdfs that txt files are built from >1 pdf page
+        if dir_exists:
+            file_count = len([name for name in os.listdir(txt_file_dir) if os.path.isfile(os.path.join(txt_file_dir, name))])
+            if file_count == pdf_file.page_count: #all txt files accounted for.
+                return False
+        else:
+            os.makedirs(txt_file_dir, exist_ok=True)
 
         for page_num in range(pdf_file.page_count):
             page_text = pdf_file.load_page(page_num).get_text("text")
@@ -88,7 +85,8 @@ def pdf_to_txt(file) -> bool:
 
 def main() -> None:
     global THREADS
-    THREADS = get_pdf_threads()
+    THREADS.extend(get_pdf_threads("USA"))
+    THREADS.extend(get_pdf_threads("CAN"))
     
     for thread in THREADS:
         thread.start()
